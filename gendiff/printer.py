2904d2829a51546994c0e5a47d75c3e52094a21f
Sergey Radyukhin <22dollars@mail.ru>
2022-09-08 15:22:46 +0300

    check hip print diff

--- gendiff/printer.py
+++ gendiff/printer.py
@@ -1,49 +1,61 @@
 import json
 import re
 
+def get_action(action):
+    return {
+        None: ' ',
+        'add': '+',
+        'remove': '-',
+    }[action]
 
-def get_action(data):
-    if not data.get('action'):
-        return ' '
-    return "+" if data.get('action') == 'added' else "-"
+
+def has_inner_tree(data):
+    return isinstance(data, list)
 
 
-def put(data):
-    dict_data = {}
-    if not isinstance(data, dict):
+def make_diff_stylish_tree(data):
+    if not has_inner_tree(data):
         return data
-    for key in data.keys():
-        dict_data[f'  {key}'] = put(data[key])
-    return dict_data
-
-
-def show_diff_stylish(data):
     result = {}
-    for key in data.keys():
-        action = get_action(data[key])
-        result[f'{action} {key}'] = put(data[key])
-    result = json.dumps(result, indent=4)
-    #result = result.replace('"', '')
-    #result = result.replace(',', '')
-    #result = result.replace('  }', '    }')
-    #result = re.sub(r"\n\s\s", '\n', result)
+    for item in data:
+        key, action, value = item
+        if action == 'update':
+            result[f'- {key}'] = make_diff_stylish_tree(value[0])
+            result[f'+ {key}'] = make_diff_stylish_tree(value[1])
+        else:
+            action = get_action(action)
+            result[f'{action} {key}'] = make_diff_stylish_tree(value)
     return result
 
 
-def show_diff_plain(data):
+def stringify(data):
+    result = json.dumps(data, indent=4)
+    result = result.replace('"', '')
+    result = result.replace(',', '')
+    result = result.replace('  }', '    }')
+    result = re.sub(r"\n\s\s", '\n', result)
+    return result
+
+
+def show_diff_stylish(data):
+    result = make_diff_stylish_tree(data)
+    return stringify(result)
+
+
+'''def show_diff_plain(data):
     result = ''
     for key in data.keys():
         action = key[:1]
         property = key[2:]
         result += f"Property '{property}' was {action} with {data[key]}\n"
-    return result
+    return result'''
 
 
 
 if __name__ == "__main__":
-    SIMPLE_RESULT = {'follow': {'action': 'removed', 'value': False}, 'host': {'value': 'hexlet.io'}, 'proxy': {'action': 'removed', 'value': '123.234.53.22'}, 'timeout': {'action': 'updated', 'From': 50, 'to': 20}, 'verbose': {'action': 'added', 'value': True}}
+    SIMPLE_RESULT = [('follow', 'remove', False), ('host', None, 'hexlet.io'), ('proxy', 'remove', '123.234.53.22'), ('timeout', 'update', (50, 20)), ('verbose', 'add', True)]
 
-    NESTED_RESULT = {'common': {'follow': {'action': 'added', 'value': False}, 'setting1': {'value': 'Value 1'}, 'setting2': {'action': 'removed', 'value': 200}, 'setting3': {'action': 'updated', 'From': True, 'to': None}, 'setting4': {'action': 'added', 'value': 'blah blah'}, 'setting5': {'action': 'added', 'value': {'key5': 'value5'}}, 'setting6': {'doge': {'wow': {'action': 'updated', 'From': '', 'to': 'so much'}}, 'key': {'value': 'value'}, 'ops': {'action': 'added', 'value': 'vops'}}}, 'group1': {'baz': {'action': 'updated', 'From': 'bas', 'to': 'bars'}, 'foo': {'value': 'bar'}, 'nest': {'action': 'updated', 'From': {'key': 'value'}, 'to': 'str'}}, 'group2': {'action': 'removed', 'value': {'abc': 12345, 'deep': {'id': 45}}}, 'group3': {'action': 'added', 'value': {'deep': {'id': {'number': 45}}, 'fee': 100500}}}
+    NESTED_RESULT = [('common', None, [('follow', 'add', False), ('setting1', None, 'Value 1'), ('setting2', 'remove', 200), ('setting3', 'update', (True, None)), ('setting4', 'add', 'blah blah'), ('setting5', 'add', [('key5', None, 'value5')]), ('setting6', None, [('doge', None, [('wow', 'update', ('', 'so much'))]), ('key', None, 'value'), ('ops', 'add', 'vops')])]), ('group1', None, [('baz', 'update', ('bas', 'bars')), ('foo', None, 'bar'), ('nest', 'update', ([('key', None, 'value')], 'str'))]), ('group2', 'remove', [('abc', None, 12345), ('deep', None, [('id', None, 45)])]), ('group3', 'add', [('deep', None, [('id', None, [('number', None, 45)])]), ('fee', None, 100500)])]
     '''with open('tests/fixtures/nested/result_plain', 'r') as f:
         f = f.read()
         print(f)
