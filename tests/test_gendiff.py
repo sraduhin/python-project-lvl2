697a5344664329d4daaa5526554eec7280bde0d3
Sergey Radyukhin <22dollars@mail.ru>
2022-09-13 12:33:13 +0300

    making fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,43 +5,43 @@ from gendiff.formatter.plain import main as show_diff_plain
 import pytest
 import tests.expected_data as expected
 
-@pytest.fixture
+
 def get_path(endpath):
     return 'tests/fixtures/' + endpath
 
 
 def test_parser_path():
-    pathfile = get_path(bad.path)
+    pathfile = get_path('bad.path')
     with pytest.raises(ValueError, match=f"{pathfile} doesn't exists"):
         parser(pathfile)
 
 
 def test_parser_extension():
-    pathfile = 'tests/fixtures/simple/file1.bad_ext'
+    pathfile = get_path('file1.bad_ext')
     with pytest.raises(ValueError, match='enexpected extension of the file: file1.bad_ext'):
         parser(pathfile)
 
 
 def test_parser_simple_json():
-    pathfile = 'tests/fixtures/simple/file1.json'
+    pathfile = get_path('simple/file1.json')
     data = expected.SIMPLE_PARSED_DATA
     assert parser(pathfile) == data
 
 
 def test_parser_nested_json():
-    pathfile = 'tests/fixtures/nested/file1.json'
+    pathfile = get_path('nested/file1.json')
     data = expected.NESTED_PARSED_DATA
     assert parser(pathfile) == data
 
 
 def test_parser_simple_yml():
-    pathfile = 'tests/fixtures/simple/file1.yml'
+    pathfile = get_path('simple/file1.yml')
     data = expected.SIMPLE_PARSED_DATA
     assert parser(pathfile) == data
 
 
 def test_parser_nested_yml():
-    pathfile = 'tests/fixtures/nested/file1.yaml'
+    pathfile = get_path('nested/file1.yaml')
     data = expected.NESTED_PARSED_DATA
     assert parser(pathfile) == data
 
@@ -59,28 +59,28 @@ def test_generate_diff_nested():
 
 
 def test_show_diff_simple_stylish():
-    pathfile = 'tests/fixtures/simple/result_stylish'
+    pathfile = get_path('simple/result_stylish')
     with open(pathfile, 'r') as f:
         result = f.read()
         assert show_diff_stylish(expected.SIMPLE_REPR) == result
 
 
 def test_show_diff_nested_stylish():
-    pathfile = 'tests/fixtures/nested/result_stylish'
+    pathfile = get_path('nested/result_stylish')
     with open(pathfile, 'r') as f:
         result = f.read()
         assert show_diff_stylish(expected.NESTED_REPR) == result
 
 
 def test_show_diff_simple_plain():
-    pathfile = 'tests/fixtures/simple/result_plain'
+    pathfile = get_path('simple/result_plain')
     with open(pathfile, 'r') as f:
         result = f.read()
         assert show_diff_plain(expected.SIMPLE_REPR) == result
 
 
 def test_show_diff_nested_plain():
-    pathfile = 'tests/fixtures/nested/result_plain'
+    pathfile = get_path('nested/result_plain')
     with open(pathfile, 'r') as f:
         result = f.read()
         assert show_diff_plain(expected.NESTED_REPR) == result
