b67c34d745dcbf31f9e366a89d5f0dd0fec90311
Sergey Radyukhin <22dollars@mail.ru>
2022-09-14 18:30:35 +0300

    refactoring

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,8 +7,8 @@ from gendiff import generate_diff
 def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', help='first_file')
-    parser.add_argument('second_file', help='second_file')
+    parser.add_argument('first_file', help='first_file', type=str)
+    parser.add_argument('second_file', help='second_file', type=str)
     parser.add_argument('-f', '--format',
                         help='set format of output')
 
